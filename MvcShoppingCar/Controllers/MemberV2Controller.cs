4f8c9b676f555869d0afadf44a38bfe0e3a2ac55
Alex <suchun.huang@advantech.com.tw>
2013-02-05 20:03:24 +0800

    新增密碼加密的方法

--- MvcShoppingCar/Controllers/MemberV2Controller.cs
+++ MvcShoppingCar/Controllers/MemberV2Controller.cs
@@ -6,11 +6,14 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MvcShoppingCar.Models;
+using System.Web.Security.Cryptography;
+using System.Web.Security;
 
 namespace MvcShoppingCar.Controllers
 {
     public class MemberV2Controller : Controller
     {
+        private string pwSalt = "aAPa435KMKojzvk309u0uadsifjakd23kxAFDGSnjvlk";
         private MemberContext db = new MemberContext();
 
         //
@@ -20,6 +23,41 @@ namespace MvcShoppingCar.Controllers
         {
             return View(db.Members.ToList());
         }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        //寫入會員資料
+        [HttpPost]
+        public ActionResult Register([Bind(Exclude = "RegisterOn,authcode")] Member member)
+        {
+            var chk_member = db.Members.Where(p => p.email == member.email).FirstOrDefault();
+            if (chk_member != null) {
+
+                ModelState.AddModelError("Email", "你輸入的mail已經有人註冊了");
+            }
+
+            if (ModelState.IsValid)
+            {
+                member.password = FormsAuthentication.HashPasswordForStoringInConfigFile(pwSalt + member.password, "SHA1");
+                member.RegisterOn = DateTime.Now;
+                member.authcode = Guid.NewGuid().ToString();
+                db.Members.Add(member);
+                db.SaveChanges();
+
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View();
+            }
+
+            
+        }
+
+
 
         //
         // GET: /MemberV2/Details/5
