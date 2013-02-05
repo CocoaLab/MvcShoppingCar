144657201e62f42ca0ecb24563736106d0cdb79e
Alex <suchun.huang@advantech.com.tw>
2013-02-05 13:37:38 +0800

    建立Member DBContext

---
+++ MvcShoppingCar/Controllers/MemberV2Controller.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcShoppingCar.Models;
+
+namespace MvcShoppingCar.Controllers
+{
+    public class MemberV2Controller : Controller
+    {
+        private MemberContext db = new MemberContext();
+
+        //
+        // GET: /MemberV2/
+
+        public ActionResult Index()
+        {
+            return View(db.Members.ToList());
+        }
+
+        //
+        // GET: /MemberV2/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Member member = db.Members.Find(id);
+            if (member == null)
+            {
+                return HttpNotFound();
+            }
+            return View(member);
+        }
+
+        //
+        // GET: /MemberV2/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /MemberV2/Create
+
+        [HttpPost]
+        public ActionResult Create(Member member)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Members.Add(member);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(member);
+        }
+
+        //
+        // GET: /MemberV2/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Member member = db.Members.Find(id);
+            if (member == null)
+            {
+                return HttpNotFound();
+            }
+            return View(member);
+        }
+
+        //
+        // POST: /MemberV2/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Member member)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(member).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(member);
+        }
+
+        //
+        // GET: /MemberV2/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Member member = db.Members.Find(id);
+            if (member == null)
+            {
+                return HttpNotFound();
+            }
+            return View(member);
+        }
+
+        //
+        // POST: /MemberV2/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Member member = db.Members.Find(id);
+            db.Members.Remove(member);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
